875726a54260b1e97621e6e5a2b94ff31835aaf0
Logan <157175146+llogann@users.noreply.github.com>
2024-12-17 13:54:42 -0800

    grabb time

--- Scripts/peg.gd
+++ Scripts/peg.gd
@@ -1,13 +1,24 @@
 extends Area2D
 
 @export var nearby = false
+@onready var pj: PinJoint2D = $"../PinJoint2D"
+var player
 
+func _ready() :
+	print(pj.node_b)
 
 
 func _on_body_entered(body):
 	nearby = true
-
-
+	player = body
+	
 
 func _on_body_exited(body):
 	nearby = false
+
+func _process(delta):
+	
+	if nearby && Input.is_action_pressed("grab"):
+		pj.node_b = player.get_path()
+	else:
+		pj.node_b = ""
