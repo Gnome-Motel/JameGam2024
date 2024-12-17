272a0e5383389bba942df740778ce12907c19d63
Nicolas Gatien <nicolas.gatien6@gmail.com>
2024-12-17 17:11:42 -0500

    Set Player Position

--- Scripts/peg.gd
+++ Scripts/peg.gd
@@ -11,7 +11,6 @@ func _ready() :
 func _on_body_entered(body):
 	nearby = true
 	player = body
-	
 
 func _on_body_exited(body):
 	nearby = false
@@ -20,5 +19,7 @@ func _process(delta):
 	
 	if nearby && Input.is_action_pressed("grab"):
 		pj.node_b = player.get_path()
+		player.position = global_position
+
 	else:
 		pj.node_b = ""
